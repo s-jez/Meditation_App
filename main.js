dd30744d8bfaa7e18e6eb7151b5940afdbec8635
s-jez <stanislawjez7@gmail.com>
2022-07-09 17:52:40 +0200

    Display description music - audio src

--- main.js
+++ main.js
@@ -1,19 +1,41 @@
+import { audioSrc } from "./audio-source.js";
+
 const inputs = document.querySelectorAll(".buttons input");
 const musicSection = document.querySelector(".music-section");
-const audioSrc = ["/music/01.mp3"];
-let musicIndex = 0;
 let audio = document.getElementById("audio");
-audio.src = audioSrc[musicIndex];
+let audioIndex = 0;
 
 const playMusic = () => {
+  audio.src = audioSrc[audioIndex].src;
   audio.play();
+  getElementsMusic();
 };
 const nextMusic = () => {
-  console.log("next...");
+  audioIndex++;
+  audio.src = audioSrc[audioIndex].src;
+  audio.play();
+  getElementsMusic();
+};
+const previousMusic = () => {
+  audioIndex--;
+  audio.src = audioSrc[audioIndex].src;
+  audio.play();
+  getElementsMusic();
 };
 const stopMusic = () => {
   audio.pause();
 };
+const getElementsMusic = () => {
+  musicSection.innerHTML = "";
+  let musicTitle = document.createElement("h1");
+  let musicTime = document.createElement("p");
+  musicTitle.innerHTML = audioSrc[audioIndex].title;
+  musicTime.innerHTML = audioSrc[audioIndex].time;
+  musicSection.appendChild(musicTitle);
+  musicSection.appendChild(musicTime);
+};
+
 inputs[0].addEventListener("click", playMusic);
 inputs[1].addEventListener("click", stopMusic);
 inputs[2].addEventListener("click", nextMusic);
+inputs[3].addEventListener("click", previousMusic);
