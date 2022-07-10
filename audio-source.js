c54a34658d54488c35142c3cd0fd573bf2c901ad
s-jez <stanislawjez7@gmail.com>
2022-07-10 16:29:03 +0200

    Change local files to url music.

--- audio-source.js
+++ audio-source.js
@@ -1,10 +1,10 @@
 export const audioSrc = [
   {
     title: "Relaxing Music 01",
-    src: "/music/01.mp3",
+    src: "https://megawrzuta.pl/files/a24a421c47c5e6cb859506eb51789703.mp3",
   },
   {
     title: "Relaxing Music 02",
-    src: "/music/02.mp3",
+    src: "https://megawrzuta.pl/files/7a98c36a12a9283ec4f83de6b4382cb6.mp3",
   },
 ];
