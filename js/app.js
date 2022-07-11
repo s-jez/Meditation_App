e7daf59925cfd79c58a349d58399537d425b5b95
s-jez <stanislawjez7@gmail.com>
2022-07-11 15:18:21 +0200

    change switch statement to array destructuring in app.js

--- js/app.js
+++ js/app.js
@@ -3,30 +3,25 @@ import { AudioPlayer } from "./audio-player.js";
 const inputs = [...document.querySelectorAll(".buttons input")];
 
 const audioPlayer = new AudioPlayer();
-inputs.forEach((item, index) => {
-  switch (index) {
-    case 0:
-      item.addEventListener("click", () => {
-        audioPlayer.playMusic();
-      });
-      break;
-    case 1:
-      item.addEventListener("click", () => {
-        audioPlayer.pauseMusic();
-      });
-      break;
-    case 2:
-      item.addEventListener("click", () => {
-        audioPlayer.nextMusic();
-      });
-      break;
-    case 3:
-      item.addEventListener("click", () => {
-        audioPlayer.previousMusic();
-      });
-    case 4:
-      item.addEventListener("input", (ev) => {
-        audioPlayer.setVolume(ev.target.value);
-      });
-  }
+const [
+  playAudioInput,
+  pauseAudioInput,
+  nextAudioInput,
+  previousAudioInput,
+  volumeAudioInput,
+] = inputs;
+playAudioInput.addEventListener("click", () => {
+  audioPlayer.playMusic();
+});
+pauseAudioInput.addEventListener("click", () => {
+  audioPlayer.pauseMusic();
+});
+nextAudioInput.addEventListener("click", () => {
+  audioPlayer.nextMusic();
+});
+previousAudioInput.addEventListener("click", () => {
+  audioPlayer.previousMusic();
+});
+volumeAudioInput.addEventListener("input", (ev) => {
+  audioPlayer.setVolume(ev.target.value);
 });
